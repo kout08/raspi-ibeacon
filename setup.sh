cebf0636e16801c4c7e549aea8f377cd2e47ae64
kout08 <kout08@users.noreply.github.com>
2016-12-10 16:24:16 +0000

    Refactor setup.sh

--- setup.sh
+++ setup.sh
@@ -8,26 +8,25 @@ apt_install(){
 	fi
 }
 
-# ex) wget_insatll "$HOME/ibeacon" "bluez-5.43.tar.xz" "https://www.kernel.org/pub/linux/bluetooth/bluez-5.43.tar.xz"
-# $1: Install dir name
-# $2: Install src name
-# $3: Install URL
+# ex) wget_insatll "bluez-5.43.tar.xz" "https://www.kernel.org/pub/linux/bluetooth/bluez-5.43.tar.xz"
+# $1: Install src name
+# $2: Install URL
 wget_install(){
+	# $DIR: relative path of this project repo
 	DIR=$(dirname $0)
 	if [ -e "$DIR/$1" ]; then
 		echo "$1 is already exist"
 	else
 		echo "$1 is not exist. Start install..."
-		#wget https://www.kernel.org/pub/linux/bluetooth/bluez-5.43.tar.xz
 		wget $2 -P $DIR
 	fi
 }
 
-# ex) open_tar_xz "$DIR" "bluez-5.43.tar.xz" "bluez-5.43"
-# $1: Open dir name
-# $2: Open tar.xz file name
-# $3: Open src name
+# ex) open_tar_xz "bluez-5.43.tar.xz" "bluez-5.43"
+# $1: Open tar.xz file name
+# $2: Open src name
 open_tar_xz(){
+	# $DIR: relative path of this project repo
 	DIR=$(dirname $0)
 	if [ -e "$DIR/$2" ]; then
 		echo "$2 is already exist"
